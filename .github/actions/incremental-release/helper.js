c0628a25fdcdbcff128ea1e2e533b55a25ea74f8
Dave Lockhart <dave.lockhart@d2l.com>
2020-11-26 11:54:54 -0500

    switching to a JS script

---
+++ .github/actions/incremental-release/helper.js
@@ -0,0 +1,15 @@
+import { Octokit } from "@octokit/rest";
+
+const lastCommitMessage = process.argv[2];
+const defaultIncrement = process.argv[3];
+const dryRun = process.argv[4];
+
+console.log(`Last commit: ${lastCommitMessage}`);
+console.log(`Default increment: ${defaultIncrement}`);
+console.log(`Dry run? ${dryRun}`);
+
+const octokit = new Octokit({
+	auth: process.env.GITHUB_TOKEN
+});
+
+process.exit();
