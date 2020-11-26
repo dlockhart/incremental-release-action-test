3e400b1e4667f6f85a8c17d8c653bd8e871538c4
Dave Lockhart <dave.lockhart@d2l.com>
2020-11-26 14:15:10 -0500

    move commit bit around

--- .github/actions/incremental-release/create-release.js
+++ .github/actions/incremental-release/create-release.js
@@ -1,7 +1,11 @@
 const { Octokit } = require('@octokit/core');
 
+const release = process.argv[2];
+
 const octokit = new Octokit({
 	auth: process.env.GITHUB_TOKEN
 });
+
+console.log(`Creating release ${release}...`)
 
 process.exit();
